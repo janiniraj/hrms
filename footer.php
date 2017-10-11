54ef69dc160edf2da2b386dbab7b2ede2ce71d71
janiniraj <janiniraj1992@gmail.com>
2017-10-11 20:18:49 +0800

    footer change

--- footer.php
+++ footer.php
@@ -142,7 +142,7 @@
 
 
 	</script>
-	<?php include 'popups.php';?>
+	<?php //include 'popups.php'; // To remove verify process?>
 	<?php include APP_BASE_PATH.'js/bootstrapDataTable.php';?>
     </body>
 </html>
