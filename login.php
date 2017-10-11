4ca905a983df309d8a067992a4cded52099569cd
janiniraj <janiniraj1992@gmail.com>
2017-10-12 02:04:49 +0800

    theme changes

--- login.php
+++ login.php
@@ -114,268 +114,212 @@ if (empty($user)) {
 $tuser = \Utils\SessionUtils::getSessionObject('user');
 $logoFileUrl = \Classes\UIManager::getInstance()->getCompanyLogoUrl();
 
-?><!DOCTYPE html>
-<html lang="en">
+?>
+<!DOCTYPE html>
+<html>
 <head>
-	<meta charset="utf-8">
-	<title><?=APP_NAME?> Login</title>
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<meta name="description" content="">
-	<meta name="author" content="">
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title><?=APP_NAME?> Login</title>
+    <!-- Tell the browser to be responsive to screen width -->
+    <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
+    <!-- Bootstrap 3.3.7 -->
+    <link rel="stylesheet" href="../theme/bower_components/bootstrap/dist/css/bootstrap.min.css">
+    <!-- Font Awesome -->
+    <link rel="stylesheet" href="../theme/bower_components/font-awesome/css/font-awesome.min.css">
+    <!-- Ionicons -->
+    <link rel="stylesheet" href="../theme/bower_components/Ionicons/css/ionicons.min.css">
+    <!-- Theme style -->
+    <link rel="stylesheet" href="../theme/dist/css/AdminLTE.min.css">
+    <!-- iCheck -->
+    <link rel="stylesheet" href="../theme/plugins/iCheck/square/blue.css">
 
-	<!-- Le styles -->
-	<link href="<?=BASE_URL?>bootstrap/css/bootstrap.css" rel="stylesheet">
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+    <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+    <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
 
-	<script type="text/javascript" src="<?=BASE_URL?>js/jquery-1.8.1.js"></script>
-	<script src="<?=BASE_URL?>bootstrap/js/bootstrap.js"></script>
-	<script src="<?=BASE_URL?>js/jquery.placeholder.js"></script>
-	<script src="<?=BASE_URL?>js/jquery.dataTables.js"></script>
-	<script src="<?=BASE_URL?>js/bootstrap-datepicker.js"></script>
-	<link href="<?=BASE_URL?>bootstrap/css/bootstrap-responsive.css" rel="stylesheet">
-	<link href="<?=BASE_URL?>css/DT_bootstrap.css?v=0.4" rel="stylesheet">
-	<link href="<?=BASE_URL?>css/datepicker.css" rel="stylesheet">
-	<link href="<?=BASE_URL?>css/style.css?v=<?=$cssVersion?>" rel="stylesheet">
-
-	<!-- Le HTML5 shim, for IE6-8 support of HTML5 elements -->
-	<!--[if lt IE 9]>
-	<script src="<?=BASE_URL?>js/html5.js"></script>
-	<![endif]-->
-
-	<style type="text/css">
-		/* Override some defaults */
-		html, body {
-			background-color: #829AA8;
-		}
-		body {
-			padding-top: 40px;
-		}
-		.container {
-			width: 300px;
-		}
-
-		/* The white background content wrapper */
-		.container > .content {
-			min-height: 0px !important;
-			background-color: #fff;
-			padding: 20px;
-			margin: 0 -20px;
-			-webkit-border-radius:0px;
-			-moz-border-radius:0px;
-			border-radius: 0px;
-			-webkit-box-shadow: 0 1px 2px rgba(0,0,0,.15);
-			-moz-box-shadow: 0 1px 2px rgba(0,0,0,.15);
-			box-shadow: 0 1px 2px rgba(0,0,0,.15);
-		}
-
-		.login-form {
-			margin-left: 65px;
-		}
-
-		legend {
-			margin-right: -50px;
-			font-weight: bold;
-			color: #404040;
-		}
-
-		.add-on{
-			-webkit-border-radius:0px;
-			-moz-border-radius:0px;
-			border-radius: 0px;
-		}
-
-		input{
-			-webkit-border-radius:0px;
-			-moz-border-radius:0px;
-			border-radius: 0px;
-		}
-
-	</style>
-
-
+    <!-- Google Font -->
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,600,700,300italic,400italic,600italic">
 </head>
+<body class="hold-transition login-page">
+<div class="login-box">
+    <div class="login-logo">
+        <a href="login.php"><b>HRMS</b></a>
+    </div>
+    <!-- /.login-logo -->
+    <div class="login-box-body">
+        <p class="login-box-msg">Sign in to start your session</p>
 
-<body>
+        <?php if (!isset($_REQUEST['cp'])) {?>
+        <form id="loginForm" action="login.php" method="post">
+            <div class="form-group has-feedback">
+                <input type="text" class="form-control" id="username" name="username" placeholder="Username">
+                <span class="glyphicon glyphicon-envelope form-control-feedback"></span>
+            </div>
+            <div class="form-group has-feedback">
+                <input type="password" class="form-control" type="password" id="password" name="password" placeholder="Password">
+                <span class="glyphicon glyphicon-lock form-control-feedback"></span>
+            </div>
+            <?php if (isset($_REQUEST['f'])) {?>
+                <div class="clearfix alert alert-error" style="font-size:11px;margin-bottom: 5px;">
+                    Login failed
+                    <?php if (isset($_REQUEST['fm'])) {
+                        echo $_REQUEST['fm'];
+                    }?>
+                </div>
+            <?php } ?>
+            <?php if (isset($_REQUEST['c'])) {?>
+                <div class="clearfix alert alert-info" style="font-size:11px;margin-bottom: 5px;">
+                    Password changed successfully
+                </div>
+            <?php } ?>
+            <div class="row">
+                <div class="col-xs-8">
+                    <div class="checkbox icheck">
+                        <label>
+                            <input type="checkbox" id="remember" name="remember" checked> Remember Me
+                        </label>
+                    </div>
+                </div>
+                <!-- /.col -->
+                <div class="col-xs-4">
+                    <button type="button" onclick="submitLogin();return false;" class="btn btn-primary btn-block btn-flat">Sign In</button>
+                </div>
+                <!-- /.col -->
+            </div>
 
+            <a href="#" onclick="showForgotPassword();return false;" >I forgot my password</a>
+        </form>
+        <form id="requestPasswordChangeForm" style="display:none;" action="">
+            <div class="form-group has-feedback">
+                <input class="form-control" type="text" id="usernameChange" name="usernameChange" placeholder="Username or Email">
+                <span class="glyphicon glyphicon-envelope form-control-feedback"></span>
+            </div>
+            <div id="requestPasswordChangeFormAlert" class="clearfix alert alert-info" style="display:none;"></div>
+            <div class="row">
+                <div class="col-xs-8 col-xs-offset-2"">
+                    <button onclick="requestPasswordChange();return false;" type="button" class="btn btn-primary btn-block btn-flat">Request Password Change</button>
+                </div>
+            </div>
+
+        </form>
+        <?php } else {?>
+            <form id="newPasswordForm" action="">
+                <fieldset>
+                    <div class="clearfix">
+                        <div class="input-prepend">
+                            <span class="add-on"><i class="icon-lock"></i></span>
+                            <input class="span2" type="password" id="password" name="password" placeholder="New Password">
+                        </div>
+                    </div>
+                    <div id="newPasswordFormAlert" class="clearfix alert alert-error" style="font-size:11px;width:147px;margin-bottom: 5px;display:none;">
+
+                    </div>
+                    <button class="btn" style="margin-top: 5px;" type="button" onclick="changePassword();return false;">Change Password&nbsp;&nbsp;<span class="icon-arrow-right"></span></button>
+                </fieldset>
+            </form>
+        <?php }?>
+        <!-- /.social-auth-links -->
+
+    </div>
+    <!-- /.login-box-body -->
+</div>
+<!-- /.login-box -->
+
+<!-- jQuery 3 -->
+<script src="../theme/bower_components/jquery/dist/jquery.min.js"></script>
+<!-- Bootstrap 3.3.7 -->
+<script src="../theme/bower_components/bootstrap/dist/js/bootstrap.min.js"></script>
+<!-- iCheck -->
+<script src="../theme/plugins/iCheck/icheck.min.js"></script>
 <script>
-	(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-			(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-		m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-	})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-
-	ga('create', '<?=\Classes\BaseService::getInstance()->getGAKey()?>', 'gamonoid.com');
-	ga('send', 'pageview');
-
+    $(function () {
+        $('input').iCheck({
+            checkboxClass: 'icheckbox_square-blue',
+            radioClass: 'iradio_square-blue',
+            increaseArea: '20%' // optional
+        });
+    });
 </script>
-
 <script type="text/javascript">
-	var key = "";
-	<?php if (isset($_REQUEST['key'])) {?>
-	key = '<?=$_REQUEST['key']?>';
-	key = key.replace(/ /g,"+");
-	<?php }?>
+    var key = "";
+    <?php if (isset($_REQUEST['key'])) {?>
+    key = '<?=$_REQUEST['key']?>';
+    key = key.replace(/ /g,"+");
+    <?php }?>
 
-	$(document).ready(function() {
-		$(window).keydown(function(event){
-			if(event.keyCode == 13) {
-				event.preventDefault();
-				return false;
-			}
-		});
+    $(document).ready(function() {
+        $(window).keydown(function(event){
+            if(event.keyCode == 13) {
+                event.preventDefault();
+                return false;
+            }
+        });
 
-		$("#password").keydown(function(event){
-			if(event.keyCode == 13) {
-				submitLogin();
-				return false;
-			}
-		});
-	});
+        $("#password").keydown(function(event){
+            if(event.keyCode == 13) {
+                submitLogin();
+                return false;
+            }
+        });
+    });
 
-	function showForgotPassword(){
-		$("#loginForm").hide();
-		$("#requestPasswordChangeForm").show();
-	}
+    function showForgotPassword(){
+        $("#loginForm").hide();
+        $("#requestPasswordChangeForm").show();
+    }
 
-	function requestPasswordChange(){
-		$("#requestPasswordChangeFormAlert").hide();
-		var id = $("#usernameChange").val();
-		$.post("service.php", {'a':'rpc','id':id}, function(data) {
-			if(data.status == "SUCCESS"){
-				$("#requestPasswordChangeFormAlert").show();
-				$("#requestPasswordChangeFormAlert").html(data.message);
-			}else{
-				$("#requestPasswordChangeFormAlert").show();
-				$("#requestPasswordChangeFormAlert").html(data.message);
-			}
-		},"json");
-	}
+    function requestPasswordChange(){
+        $("#requestPasswordChangeFormAlert").hide();
+        var id = $("#usernameChange").val();
+        $.post("service.php", {'a':'rpc','id':id}, function(data) {
+            if(data.status == "SUCCESS"){
+                $("#requestPasswordChangeFormAlert").show();
+                $("#requestPasswordChangeFormAlert").html(data.message);
+            }else{
+                $("#requestPasswordChangeFormAlert").show();
+                $("#requestPasswordChangeFormAlert").html(data.message);
+            }
+        },"json");
+    }
 
-	function changePassword(){
-		$("#newPasswordFormAlert").hide();
-		var password = $("#password").val();
+    function changePassword(){
+        $("#newPasswordFormAlert").hide();
+        var password = $("#password").val();
 
-		var passwordValidation =  function (str) {
-			var val = /^[a-zA-Z0-9]\w{6,}$/;
-			return str != null && val.test(str);
-		};
-
-
-		if(!passwordValidation(password)){
-			$("#newPasswordFormAlert").show();
-			$("#newPasswordFormAlert").html("Password may contain only letters, numbers and should be longer than 6 characters");
-			return;
-		}
+        var passwordValidation =  function (str) {
+            var val = /^[a-zA-Z0-9]\w{6,}$/;
+            return str != null && val.test(str);
+        };
 
 
-		$.post("service.php", {'a':'rsp','key':key,'pwd':password,"now":"1"}, function(data) {
-			if(data.status == "SUCCESS"){
-				top.location.href = "login.php?c=1";
-			}else{
-				$("#newPasswordFormAlert").show();
-				$("#newPasswordFormAlert").html(data.message);
-			}
-		},"json");
-	}
+        if(!passwordValidation(password)){
+            $("#newPasswordFormAlert").show();
+            $("#newPasswordFormAlert").html("Password may contain only letters, numbers and should be longer than 6 characters");
+            return;
+        }
 
-	function submitLogin(){
-		try{
-			localStorage.clear();
-		}catch(e){}
-		$("#loginForm").submit();
-	}
+
+        $.post("service.php", {'a':'rsp','key':key,'pwd':password,"now":"1"}, function(data) {
+            if(data.status == "SUCCESS"){
+                top.location.href = "login.php?c=1";
+            }else{
+                $("#newPasswordFormAlert").show();
+                $("#newPasswordFormAlert").html(data.message);
+            }
+        },"json");
+    }
+
+    function submitLogin(){
+        try{
+            localStorage.clear();
+        }catch(e){}
+        $("#loginForm").submit();
+    }
 
 </script>
-<div class="container">
-	<?php if (defined('DEMO_MODE')) {?>
-		<div class="content" style="top: 30px;
-            position: absolute;
-            left: 50px;
-            width: 380px;
-            height: 100px;">
-
-			<ul class="list-group" style="font-size:12px;">
-				<li style="padding-bottom:3px;" class="list-group-item">Admin: (Username = admin/ Password = admin)</li>
-				<li style="padding-bottom:3px;" class="list-group-item">Manager: (Username = manager/ Password = demouserpwd)</li>
-				<li style="padding-bottom:3px;" class="list-group-item">User: (Username = user1/ Password = demouserpwd)</li>
-				<li style="padding-bottom:3px;" class="list-group-item">User: (Username = user2/ Password = demouserpwd)</li>
-			</ul>
-		</div>
-	<?php }?>
-	<div class="content" style="margin-top:100px;">
-		<div class="row">
-			<div class="login-form">
-				<h2><img src="<?=$logoFileUrl?>"/></h2>
-				<?php if (!isset($_REQUEST['cp'])) {?>
-					<form id="loginForm" action="login.php" method="POST">
-						<fieldset>
-							<div class="clearfix">
-								<div class="input-prepend">
-									<span class="add-on"><i class="icon-user"></i></span>
-									<input class="span2" type="text" id="username" name="username" placeholder="Username">
-								</div>
-							</div>
-							<div class="clearfix">
-								<div class="input-prepend">
-									<span class="add-on"><i class="icon-lock"></i></span>
-									<input class="span2" type="password" id="password" name="password" placeholder="Password">
-								</div>
-							</div>
-							<div class="clearfix">
-								<div class="checkbox">
-									<label><input id="remember" name="remember" type="checkbox" value="remember" checked>Remember me</label>
-								</div>
-							</div>
-							<?php if (isset($_REQUEST['f'])) {?>
-								<div class="clearfix alert alert-error" style="font-size:11px;width:147px;margin-bottom: 5px;">
-									Login failed
-									<?php if (isset($_REQUEST['fm'])) {
-										echo $_REQUEST['fm'];
-									}?>
-								</div>
-							<?php } ?>
-							<?php if (isset($_REQUEST['c'])) {?>
-								<div class="clearfix alert alert-info" style="font-size:11px;width:147px;margin-bottom: 5px;">
-									Password changed successfully
-								</div>
-							<?php } ?>
-							<button class="btn" style="margin-top: 5px;" type="button" onclick="submitLogin();return false;">Sign in&nbsp;&nbsp;<span class="icon-arrow-right"></span></button>
-						</fieldset>
-						<div class="clearfix">
-							<a href="" onclick="showForgotPassword();return false;" style="float:left;margin-top: 10px;">Forgot password</a>
-						</div>
-					</form>
-					<form id="requestPasswordChangeForm" style="display:none;" action="">
-						<fieldset>
-							<div class="clearfix">
-								<div class="input-prepend">
-									<span class="add-on"><i class="icon-user"></i></span>
-									<input class="span2" type="text" id="usernameChange" name="usernameChange" placeholder="Username or Email">
-								</div>
-							</div>
-							<div id="requestPasswordChangeFormAlert" class="clearfix alert alert-info" style="font-size:11px;width:147px;margin-bottom: 5px;display:none;">
-
-							</div>
-							<button class="btn" style="margin-top: 5px;" type="button" onclick="requestPasswordChange();return false;">Request Password Change&nbsp;&nbsp;<span class="icon-arrow-right"></span></button>
-						</fieldset>
-					</form>
-				<?php } else {?>
-					<form id="newPasswordForm" action="">
-						<fieldset>
-							<div class="clearfix">
-								<div class="input-prepend">
-									<span class="add-on"><i class="icon-lock"></i></span>
-									<input class="span2" type="password" id="password" name="password" placeholder="New Password">
-								</div>
-							</div>
-							<div id="newPasswordFormAlert" class="clearfix alert alert-error" style="font-size:11px;width:147px;margin-bottom: 5px;display:none;">
-
-							</div>
-							<button class="btn" style="margin-top: 5px;" type="button" onclick="changePassword();return false;">Change Password&nbsp;&nbsp;<span class="icon-arrow-right"></span></button>
-						</fieldset>
-					</form>
-				<?php }?>
-			</div>
-		</div>
-	</div>
-</div> <!-- /container -->
 </body>
 </html>
